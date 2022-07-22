de7f96e1bf9f8a12cb1ff1ea2a565722a0af80cd
Rohit Patel <rohitpatel.1122@gmail.com>
2022-07-22 19:09:08 +0530

    Class Component

--- src/Components/HelloWorld.js
+++ src/Components/HelloWorld.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-function HelloWorld(props){
+/* function HelloWorld(props){
     return(
         <h1>Hello {props.name}</h1>
     );
+} */
+
+class HelloWorld extends React.Component {
+    render(){
+        return(
+            <h1>Hello {this.props.name}</h1>
+        )
+    }    
 }
 
 export default HelloWorld;
