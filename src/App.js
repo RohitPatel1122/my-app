e6d5b3187c9f38f6261579bc85a582a1d716b2ae
Rohit Patel <rohitpatel.1122@gmail.com>
2022-07-22 19:24:17 +0530

    Added Counter Example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import HelloWorld from './Components/HelloWorld';
+import Counter from './Components/CounterExample';
 function App() {
   return (
     <div>
-        <HelloWorld name = "Rohit patel"></HelloWorld>
+        {/* <HelloWorld name = "Rohit patel"></HelloWorld> */}
+        <Counter/>
     </div>
   );
 }
